317254fc36efe7c47e9ed4f2a17c349b55212b9d
ranutra <164624216+ranutra@users.noreply.github.com>
2024-10-03 09:15:49 +0700

    HomePage (w/Grid)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/order-pekerja', function () {
+    return 'Order Pekerja page';
+});
+
+Route::get('/review', function () {
+    return 'Review page';
+});
+
+Route::get('/about-us', function () {
+    return 'About Us page';
+});
